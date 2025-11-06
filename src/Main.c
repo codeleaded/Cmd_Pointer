88ff90c3b6a14bed8d8401f48164cf082e1f0845
C-Reaper <gnu.c.reaper@gmail.com>
2025-11-06 18:18:50 +0100

    update

--- src/Main.c
+++ src/Main.c
@@ -2,8 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "C:\Wichtig\System\Static\Library\String.h"
-#include "C:\Wichtig\System\Static\Library\Pointer.h"
+#if defined(__linux__)
+#include "/home/codeleaded/System/Static/Library/String.h"
+#include "/home/codeleaded/System/Static/Library/Pointer.h"
+#elif defined(_WINE)
+#include "/home/codeleaded/System/Static/Library/String.h"
+#include "/home/codeleaded/System/Static/Library/Pointer.h"
+#elif defined(_WIN32)
+#include "F:/home/codeleaded/System/Static/Library/String.h"
+#include "F:/home/codeleaded/System/Static/Library/Pointer.h"
+#elif defined(__APPLE__)
+#error "Apple not supported!"
+#else
+#error "Platform not supported!"
+#endif
 
 int main(void){
 
@@ -11,10 +23,8 @@ int main(void){
     SharedPointer_A sp1 = SharedPointer_New(CStr_Cpy("Hello World"));
     printf("------\n");
     SharedPointer_A sp2 = SharedPointer_Share(&sp1);
-
     {
         SharedPointer_A sp3 = SharedPointer_Share(&sp2);
-
         {
             SharedPointer_A sp4 = SharedPointer_Share(&sp3);
             printf("------\n");
@@ -26,7 +36,7 @@ int main(void){
 
     UniquePointer_A up1 = UniquePointer_New(CStr_Cpy("Hello World"));
 
-    printf("Out: %s\n",SharedPointer_Get(&sp1));
+    printf("Out: %s\n",(char*)SharedPointer_Get(&sp1));
     
     return 0;
 }
